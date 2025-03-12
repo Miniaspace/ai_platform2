b29ab7683dbba783a5729eb0fb8c1240054d66a1
lenovo <maxiaoboyu@126.com>
2025-03-12 16:58:58 +0800

    Initial commit

---
+++ aiplatform-common/aiplatform-common-core/src/main/java/com/aiplatform/common/core/constant/ServiceNameConstants.java
@@ -0,0 +1,24 @@
+package com.aiplatform.common.core.constant;
+
+/**
+ * 服务名称
+ * 
+ * @author ruoyi
+ */
+public class ServiceNameConstants
+{
+    /**
+     * 认证服务的serviceid
+     */
+    public static final String AUTH_SERVICE = "aiplatform-auth";
+
+    /**
+     * 系统模块的serviceid
+     */
+    public static final String SYSTEM_SERVICE = "aiplatform-system";
+
+    /**
+     * 文件服务的serviceid
+     */
+    public static final String FILE_SERVICE = "aiplatform-file";
+}
